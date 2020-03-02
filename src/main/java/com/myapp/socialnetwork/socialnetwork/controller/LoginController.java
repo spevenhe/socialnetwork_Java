77a9fdeac4f4cdb61dec66c674836255a7bc6616
spevenhe <spevenhe@gmail.com>
2020-03-02 23:50:01 +0800

    finished the activation email sent function , next to finish the verify function

--- src/main/java/com/myapp/socialnetwork/socialnetwork/controller/LoginController.java
+++ src/main/java/com/myapp/socialnetwork/socialnetwork/controller/LoginController.java
@@ -1,14 +1,41 @@
 package com.myapp.socialnetwork.socialnetwork.controller;
 
+import com.myapp.socialnetwork.socialnetwork.entity.User;
+import com.myapp.socialnetwork.socialnetwork.service.UserService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Map;
+
 @Controller
 public class LoginController {
+
+    @Autowired
+    private UserService userService;
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
         return "/site/register";
     }
+
+    @RequestMapping(path = "/register", method = RequestMethod.POST)
+    public String register(Model model, User user) { //如果传入参数与user的属性相匹配，就会自动传入user
+        Map<String, Object> map = userService.register(user);
+        if (map== null || map.isEmpty()) {
+            model.addAttribute("msg", "注册成功， activation email was sent, 请激活");
+            model.addAttribute("target", "/index");
+            return "/site/operate-result";
+        }
+        else {
+            model.addAttribute("usernameMsg", map.get("usernameMsg"));
+            model.addAttribute("passwordMsg", map.get("passwordMsg"));
+            model.addAttribute("emailMsg", map.get("emailMsg"));
+            return "/site/register";
+        }
+    }
+
 }
---
+++ src/main/java/com/myapp/socialnetwork/socialnetwork/util/SocialnetworkConstant.java
@@ -0,0 +1,19 @@
+package com.myapp.socialnetwork.socialnetwork.util;
+
+public interface SocialnetworkConstant {
+    /**
+     * 激活成功
+     */
+    int ACTIVATION_SUCCESS = 0;
+
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_REPEAT = 1;
+
+    /**
+     * 激活失败
+     */
+    int ACTIVATION_FAILURE = 2;
+
+}
