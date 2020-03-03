fdb06776c3325e4bd746e37a1ef9af2c01c0b997
spevenhe <spevenhe@gmail.com>
2020-03-03 21:52:38 +0800

    finished login sql mapper

--- src/test/java/com/myapp/socialnetwork/socialnetwork/MapperTests.java
+++ src/test/java/com/myapp/socialnetwork/socialnetwork/MapperTests.java
@@ -2,8 +2,10 @@ package com.myapp.socialnetwork.socialnetwork;
 
 import com.myapp.socialnetwork.socialnetwork.DAO.AlphaDao;
 import com.myapp.socialnetwork.socialnetwork.DAO.DiscussPostMapper;
+import com.myapp.socialnetwork.socialnetwork.DAO.LoginTicketMapper;
 import com.myapp.socialnetwork.socialnetwork.DAO.UserMapper;
 import com.myapp.socialnetwork.socialnetwork.entity.DiscussPost;
+import com.myapp.socialnetwork.socialnetwork.entity.LoginTicket;
 import com.myapp.socialnetwork.socialnetwork.entity.User;
 import com.myapp.socialnetwork.socialnetwork.service.AlphaService;
 import org.assertj.core.error.ShouldBeAfterYear;
@@ -32,6 +34,9 @@ public class MapperTests {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
     @Test
     public void testSelectUser() {
@@ -80,4 +85,27 @@ public class MapperTests {
         int rows = discussPostMapper.selectDiscussPostRows(149);
         System.out.println(rows);
     }
+
+    @Test
+    public void testInsertLoginTicket() {
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(101);
+        loginTicket.setTicket("abc");
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis()+ 1000*60*10));
+
+        loginTicketMapper.insertLoginTicket(loginTicket);
+    }
+
+    @Test
+    public void testSelectLoginTicket() {
+        LoginTicket loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+        loginTicketMapper.updateStatus("abc",0);
+        loginTicket = loginTicketMapper.selectByTicket("abc");
+        System.out.println(loginTicket);
+
+    }
+
 }
