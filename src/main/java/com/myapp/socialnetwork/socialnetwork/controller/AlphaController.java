1ec3019b77b6b71ff5bfdba0e8f80dcb207d4091
spevenhe <spevenhe@gmail.com>
2020-03-03 18:10:08 +0800

    finish cookie and session

--- src/main/java/com/myapp/socialnetwork/socialnetwork/controller/AlphaController.java
+++ src/main/java/com/myapp/socialnetwork/socialnetwork/controller/AlphaController.java
@@ -2,14 +2,18 @@ package com.myapp.socialnetwork.socialnetwork.controller;
 
 import com.myapp.socialnetwork.socialnetwork.DAO.AlphaDao;
 import com.myapp.socialnetwork.socialnetwork.service.AlphaService;
+import com.myapp.socialnetwork.socialnetwork.util.SocialnetworkUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.validation.Path;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -129,4 +133,42 @@ public class AlphaController {
         emps.add(emp2);
         return emps;
     }
+
+    //cookie example
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        // 创建cookie
+        Cookie cookie = new Cookie("code", SocialnetworkUtil.generateUUID());
+        // 设置cookie生效范围
+        cookie.setPath("/socialnetwork/alpha");
+        // 设置cookie生存时间
+        cookie.setMaxAge(60*10);
+        // 发送cookie
+        response.addCookie(cookie);
+        return "set cookie";
+    }
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code) {
+        System.out.println(code);
+        return "get cookie";
+    }
+    // session示例
+    @RequestMapping(path = "/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("id",1);
+        session.setAttribute("name","Test");
+        return "set session";
+    }
+
+    @RequestMapping(path = "/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "get session";
+    }
+
 }
