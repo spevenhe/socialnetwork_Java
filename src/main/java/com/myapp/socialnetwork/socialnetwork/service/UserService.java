ecb25ff1f1d224c9c6311f4fa0c170c456a94a54
spevenhe <spevenhe@gmail.com>
2020-03-04 21:55:01 +0800

    finished login and logout

--- src/main/java/com/myapp/socialnetwork/socialnetwork/service/UserService.java
+++ src/main/java/com/myapp/socialnetwork/socialnetwork/service/UserService.java
@@ -1,6 +1,8 @@
 package com.myapp.socialnetwork.socialnetwork.service;
 
+import com.myapp.socialnetwork.socialnetwork.DAO.LoginTicketMapper;
 import com.myapp.socialnetwork.socialnetwork.DAO.UserMapper;
+import com.myapp.socialnetwork.socialnetwork.entity.LoginTicket;
 import com.myapp.socialnetwork.socialnetwork.entity.User;
 import com.myapp.socialnetwork.socialnetwork.util.MailClient;
 import com.myapp.socialnetwork.socialnetwork.util.SocialnetworkConstant;
@@ -35,6 +37,9 @@ public class UserService implements SocialnetworkConstant {
 
     @Value("${server.servlet.context-path}")
     private String contextPath;
+
+    @Autowired
+    private LoginTicketMapper loginTicketMapper;
 
 
     public User findUserById(int id) {
@@ -103,5 +108,50 @@ public class UserService implements SocialnetworkConstant {
         } else{
             return ACTIVATION_FAILURE;
         }
+    }
+
+    public Map<String, Object> login(String username, String password, int expiredSeconds) {
+        Map<String, Object>map = new HashMap<>();
+        // null value
+        if(StringUtils.isBlank(username)){
+            map.put("usernameMsg","账号不能为空");
+            return map;
+        }
+        if(StringUtils.isAllBlank(password)){
+            map.put("passwordMsg","密码不能为空");
+            return map;
+        }
+
+        // 验证账号
+        User user = userMapper.selectByName(username);
+        if(user == null){
+            map.put("usernameMsg","此账号不存在");
+            return map;
+        }
+        // 验证状态
+        if(user.getStatus() == 0){
+            map.put("usernameMsg","此账号未激活");
+            return map;
+        }
+        // 验证密码
+        password = SocialnetworkUtil.md5(password + user.getSalt());
+        if(!user.getPassword().equals(password)){
+            map.put("passwordMsg","密码不正确");
+            return map;
+        }
+        // 生成登陆凭证
+        LoginTicket loginTicket = new LoginTicket();
+        loginTicket.setUserId(user.getId());
+        loginTicket.setTicket(SocialnetworkUtil.generateUUID());
+        loginTicket.setStatus(0);
+        loginTicket.setExpired(new Date(System.currentTimeMillis() + expiredSeconds*1000));
+        loginTicketMapper.insertLoginTicket(loginTicket);
+
+        map.put("ticket", loginTicket.getTicket());
+        return map;
+    }
+
+    public void logout(String ticket) {
+        loginTicketMapper.updateStatus(ticket,1);
     }
 }
--- src/main/java/com/myapp/socialnetwork/socialnetwork/util/SocialnetworkConstant.java
+++ src/main/java/com/myapp/socialnetwork/socialnetwork/util/SocialnetworkConstant.java
@@ -16,4 +16,14 @@ public interface SocialnetworkConstant {
      */
     int ACTIVATION_FAILURE = 2;
 
+    /**
+     * 默认状态登录凭证的超时时间
+     */
+    int DEAFULT_EXPIRED_SECONDS = 3600*12;
+
+    /**
+     * 记住状态登录凭证超时时间
+     */
+    int REMEMBER_EXPIRED_SECONDS = 3600*24*100;
+
 }
