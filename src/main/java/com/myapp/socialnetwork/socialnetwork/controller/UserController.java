88cec86915817f82d4361560e5f5f9237d1742dd
spevenhe <spevenhe@gmail.com>
2020-03-07 22:12:57 +0800

    finished the login required and user header

--- src/main/java/com/myapp/socialnetwork/socialnetwork/controller/UserController.java
+++ src/main/java/com/myapp/socialnetwork/socialnetwork/controller/UserController.java
@@ -1,15 +1,113 @@
 package com.myapp.socialnetwork.socialnetwork.controller;
 
+import com.myapp.socialnetwork.socialnetwork.annotation.LoginRequired;
+import com.myapp.socialnetwork.socialnetwork.entity.User;
+import com.myapp.socialnetwork.socialnetwork.service.UserService;
+import com.myapp.socialnetwork.socialnetwork.util.HostHolder;
+import com.myapp.socialnetwork.socialnetwork.util.SocialnetworkUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Value("${socialnetwork.path.upload}")
+    private String uploadPath;
+
+    @Value("${socialnetwork.path.domain}")
+    private String domain;
+
+    @Value("${server.servlet.context-path}")
+    private String contextPath;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
     public String getSettingPage() {
         return "/site/setting";
     }
+
+    @LoginRequired
+    @RequestMapping(path = "/upload", method = RequestMethod.POST)
+    public String uploadHeader(MultipartFile headerImage, Model model) {
+        if(headerImage== null){
+            model.addAttribute("error", "还没有选择图片");
+            return "/site/setting";
+        }
+
+        String fileName = headerImage.getOriginalFilename();
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        if(StringUtils.isBlank(suffix)) {
+            model.addAttribute("error", "文件格式不正确");
+            return "/site/setting";
+        }
+        // 生成随机文件名
+        fileName = SocialnetworkUtil.generateUUID() + suffix;
+        //确定存放路径
+        File dest = new File(uploadPath+ "/" + fileName);
+        try {
+            headerImage.transferTo(dest);
+        } catch (IOException e) {
+            logger.error("上传文件失败：" + e.getMessage());
+            throw new RuntimeException("上传文件失败，服务器异常", e);
+        }
+        // 更新当前用户访问头像的路径
+        // http://localhost:8080/socialnetwork/user/header/xx.png
+        User user = hostHolder.getUser();
+        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+        userService.updateHeader(user.getId(),headerUrl);
+
+        return "redirect:/index";
+
+    }
+
+    @RequestMapping(path = "/header/{fileName}", method = RequestMethod.GET)
+    public void getHeader(@PathVariable("fileName") String fileName, HttpServletResponse response) {
+        // 服务器存放路径
+        fileName = uploadPath + "/" +fileName;
+        // 文件的后缀
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        // 响应图片
+        response.setContentType("image/"+suffix);
+        try (
+                FileInputStream fis = new FileInputStream(fileName);
+                OutputStream os = response.getOutputStream();){
+
+
+            byte[] buffer = new byte[1024];
+            int b = 0;
+            while ((b=fis.read(buffer))!=-1){
+                os.write(buffer,0,b);
+            }
+        } catch (IOException e) {
+            logger.error("读取头像失败" + e.getMessage());
+        }
+    }
+
+
+
 }
